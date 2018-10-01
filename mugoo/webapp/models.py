0998df3d705c0cd11d8b86a5de8351ce0251537d
Siddharth Kala <siddharth.kala1989@gmail.com>
2018-10-01 20:41:22 +0530

    Update models.py

--- mugoo/webapp/models.py
+++ mugoo/webapp/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from django.urls import reverse
-# Create your models here.
+import uuid # Required for unique book instances
+from datetime import date
 class Book(models.Model):
     title=models.CharField(max_length=200,unique=True)
     version = models.CharField(max_length=20,blank=True)
     author = models.CharField(max_length=200)
     isbn = models.CharField(max_length=100,blank=True)
-    postedon = models.DateTimeField()
 
     def get_absolute_url(self):
         """
@@ -35,7 +35,38 @@ from django.contrib.auth.models import User
 class UserProfile(models.Model):
     address_1 = models.CharField(_("address"), max_length=128)
     address_2 = models.CharField(_("address cont'd"), max_length=128, blank=True)
-    area = models.ForeignKey(Area)
-    user=models.OneToOneField(User,related_name='user')
+    area = models.ForeignKey(Area,on_delete=models.SET_NULL,null=True)
+    user=models.OneToOneField(User,related_name='user',on_delete=models.SET_NULL,null=True)
     def __str__(self):
-        return self.user.name
+        return self.user.username
+
+
+class BookInstance(models.Model):
+    """
+    Model representing a specific copy of a book (i.e. that can be borrowed from the library).
+    """
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4,
+                          help_text="Unique ID for this particular book across whole library")
+    book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
+    user = models.ForeignKey('UserProfile', on_delete=models.SET_NULL, null=True)
+    postedon = models.DateTimeField()
+
+    ACTION_STATUS = (
+        ('sell', 'Sell'),
+        ('rent', 'Rent'),
+        ('buy', 'Buy'),
+        ('borrow', 'Borrow'),
+    )
+
+    role = models.CharField(max_length=6, choices=ACTION_STATUS, help_text='task')
+
+    class Meta:
+        ordering = ["postedon"]
+        permissions = (("authorized_user", "only authorized user"),)
+
+    def __str__(self):
+        """
+        String for representing the Model object.
+        """
+        # return '%s (%s)' % (self.id,self.book.title)
+        return '{0} ({1})'.format(self.user, self.book)
