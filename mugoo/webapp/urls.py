c184296993ddd419dedf6ba3e512bf8ea95c6999
Siddharth Kala <siddharth.kala1989@gmail.com>
2018-10-01 20:41:48 +0530

    Update urls.py

--- mugoo/webapp/urls.py
+++ mugoo/webapp/urls.py
@@ -2,4 +2,6 @@ from django.conf.urls import url
 from .views import *
 urlpatterns = [
     url(r'^create_areas/', create_areas),
+    url(r'^search/$', BookList.as_view(), name='search'),
+    url(r'', index, name='index'),
 ]
